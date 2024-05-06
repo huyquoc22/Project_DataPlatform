8040be28c355457afd9be278aee4cc0aea81c175
QuocHuy <huyvo8500@email.com>
2024-05-06 19:49:43 +0700

    final

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,8 @@ import joblib
 import numpy as np
 from pydantic import BaseModel
 
-
-
 app = FastAPI(
     title="Credit Card Fraud Detection API",
-    description="""An API that utilises a Machine Learning model that detects if a credit card transaction is fraudulent or not based on the following features: hours, amount, transaction type etc.""",
     version="1.0.0", debug=True)
 
 
@@ -30,24 +27,50 @@ async def favicon():
     return FileResponse(favicon_path)
 																	
 class fraudDetection(BaseModel):
-    step:int
-    types:int
-    amount:float	
-    oldbalanceorig:float	
-    newbalanceorig:float	
-    oldbalancedest:float	
-    newbalancedest:float	
-    isflaggedfraud:float
-
+    time:float
+    V1:float
+    V2:float
+    V3:float
+    V4:float
+    V5:float
+    V6:float
+    V7:float
+    V8:float
+    V9:float
+    V10:float
+    V11:float
+    V12:float
+    V13:float
+    V14:float
+    V15:float
+    V16:float
+    V17:float
+    V18:float
+    V19:float
+    V20:float
+    V21:float
+    V22:float
+    V23:float
+    V24:float
+    V25:float
+    V26:float
+    V27:float
+    V28:float
+    Amount:float
 
 @app.post('/predict')
-def predict(data : fraudDetection):
-                                                                                                                                                                                                                                
-    features = np.array([[data.step, data.types, data.amount, data.oldbalanceorig, data.newbalanceorig, data.oldbalancedest, data.newbalancedest, data.isflaggedfraud]])
-    model = joblib.load('credit_fraud.pkl')
+def predict(data: fraudDetection):
+    # Prepare features for prediction
+    features = np.array([[data.time, data.V1, data.V2, data.V3, data.V4, data.V5, data.V6, data.V7,
+                          data.V8, data.V9, data.V10, data.V11, data.V12, data.V13, data.V14, data.V15, 
+                          data.V16, data.V17, data.V18, data.V19, data.V20, data.V21, data.V22, data.V23, 
+                          data.V24, data.V25, data.V26, data.V27, data.V28, data.Amount]])
 
+    # Perform prediction using the loaded model
     predictions = model.predict(features)
+
+    # Return prediction result
     if predictions == 1:
-        return {"fraudulent"}
-    elif predictions == 0:
-        return {"not fraudulent"}
+        return {"Fraudulent transaction"}
+    else:
+        return {"Legitimate transaction"}
--- frontend/app.py
+++ frontend/app.py
@@ -2,101 +2,101 @@ import streamlit as st
 import json
 import requests as re
 
-st.title("Credit Card Fraud Detection Web App")
+st.title("Hệ thống phát hiện gian lận thẻ tín dụng thời gian thực")
 
 st.image("image.png")
 
 st.write("""
-## About
-Credit card fraud is a form of identity theft that involves an unauthorized taking of another's credit card information for the purpose of charging purchases to the account or removing funds from it.
 
-**This Streamlit App utilizes a Machine Learning API in order to detect fraudulent credit card  based on the following criteria: hours, type of transaction, amount, balance before and after transaction etc.** 
-
-The notebook, model and documentation(Dockerfiles, FastAPI script, Streamlit App script) are available on [GitHub.](https://github.com/Nneji123/Credit-Card-Fraud-Detection)        
-
-**Made by Group 3 Zummit Africa AI/ML Team**
-
-**Contributors:** 
-- **Hilary Ifezue(Group Lead)**
-- **Nneji Ifeanyi**
-- **Somtochukwu Ogechi**
-- **ThankGod Omieje**
+**Thành viên - MSSV** 
+- **Võ Quốc Huy - 20081001**
+- **Ngô Hoàng Nhật Huy - 20079071**
+- **Nguyễn Quang Bảo -  20083601**
+- **Nguyễn Xuân Giang - 20079601**
 """)
 
-
 st.sidebar.header('Input Features of The Transaction')
-
-sender_name = st.sidebar.text_input("""Input Sender ID""")
-receiver_name = st.sidebar.text_input("""Input Receiver ID""")
-step = st.sidebar.slider("""Number of Hours it took the Transaction to complete: """)
-types = st.sidebar.subheader(f"""
-                 Enter Type of Transfer Made:\n\n\n\n
-                 0 for 'Cash In' Transaction\n 
-                 1 for 'Cash Out' Transaction\n 
-                 2 for 'Debit' Transaction\n
-                 3 for 'Payment' Transaction\n  
-                 4 for 'Transfer' Transaction\n""")
-types = st.sidebar.selectbox("",(0,1,2,3,4))
-x = ''
-if types == 0:
-    x = 'Cash in'
-if types == 1:
-    x = 'Cash Out'
-if types == 2:
-    x = 'Debit'
-if types == 3:
-    x = 'Payment'
-if types == 4:
-    x =  'Transfer'
-    
-amount = st.sidebar.number_input("Amount in $",min_value=0, max_value=110000)
-oldbalanceorg = st.sidebar.number_input("""Original Balance Before Transaction was made""",min_value=0, max_value=110000)
-newbalanceorg= st.sidebar.number_input("""New Balance After Transaction was made""",min_value=0, max_value=110000)
-oldbalancedest= st.sidebar.number_input("""Old Balance""",min_value=0, max_value=110000)
-newbalancedest= st.sidebar.number_input("""New Balance""",min_value=0, max_value=110000)
-isflaggedfraud = st.sidebar.selectbox("""Specify if this was flagged as Fraud by your System: """,(0,1))
+time = st.sidebar.number_input("Time")
+V1 = st.sidebar.number_input("V1")
+V2 = st.sidebar.number_input("V2")
+V3 = st.sidebar.number_input("V3")
+V4 = st.sidebar.number_input("V4")
+V5 = st.sidebar.number_input("V5")
+V6 = st.sidebar.number_input("V6")
+V7 = st.sidebar.number_input("V7")
+V8 = st.sidebar.number_input("V8")
+V9 = st.sidebar.number_input("V9")
+V10 = st.sidebar.number_input("V10")
+V11 = st.sidebar.number_input("V11")
+V12 = st.sidebar.number_input("V12")
+V13 = st.sidebar.number_input("V13")
+V14 = st.sidebar.number_input("V14")
+V15 = st.sidebar.number_input("V15")
+V16 = st.sidebar.number_input("V16")
+V17 = st.sidebar.number_input("V17")
+V18 = st.sidebar.number_input("V18")
+V19 = st.sidebar.number_input("V19")
+V20 = st.sidebar.number_input("V20")
+V21 = st.sidebar.number_input("V21")
+V22 = st.sidebar.number_input("V22")
+V23 = st.sidebar.number_input("V23")
+V24 = st.sidebar.number_input("V24")
+V25 = st.sidebar.number_input("V25")
+V26 = st.sidebar.number_input("V26")
+V27 = st.sidebar.number_input("V27")
+V28 = st.sidebar.number_input("V28")
+Amount = st.sidebar.number_input("Amount")
 
 
 if st.button("Detection Result"):
     values = {
-    "step": step,
-    "types": types,
-    "amount": amount,
-    "oldbalanceorig": oldbalanceorg,
-    "newbalanceorig": newbalanceorg,
-    "oldbalancedest": oldbalancedest,
-    "newbalancedest": newbalancedest,
-    "isflaggedfraud": isflaggedfraud
+        "time": float(time),
+        "V1": float(V1),
+        "V2": float(V2),
+        "V3": float(V3),
+        "V4": float(V4),
+        "V5": float(V5),
+        "V6": float(V6),
+        "V7": float(V7),
+        "V8": float(V8),
+        "V9": float(V9),
+        "V10": float(V10),
+        "V11": float(V11),
+        "V12": float(V12),
+        "V13": float(V13),
+        "V14": float(V14),
+        "V15": float(V15),
+        "V16": float(V16),
+        "V17": float(V17),
+        "V18": float(V18),
+        "V19": float(V19),
+        "V20": float(V20),
+        "V21": float(V21),
+        "V22": float(V22),
+        "V23": float(V23),
+        "V24": float(V24),
+        "V25": float(V25),
+        "V26": float(V26),
+        "V27": float(V27),
+        "V28": float(V28),
+        "Amount": float(Amount)
     }
 
-
     st.write(f"""### These are the transaction details:\n
-    Sender ID: {sender_name}
-    Receiver ID: {receiver_name}
-    1. Number of Hours it took to complete: {step}\n
-    2. Type of Transaction: {x}\n
-    3. Amount Sent: {amount}\n
-    4. Sender Previous Balance Before Transaction: {oldbalanceorg}\n
-    5. Sender New Balance After Transaction: {newbalanceorg}\n
-    6. Recepient Balance Before Transaction: {oldbalancedest}\n
-    7. Recepient Balance After Transaction: {newbalancedest}\n
-    8. System Flag Fraud Status: {isflaggedfraud}
-                """)
+    Time: {time} \n 
+    V1:{V1} \t V2:{V2} \t V3:{V3} \t V4:{V4} \t V5:{V5} \t V6:{V6} \t V7:{V7} \n
+    V8:{V8} \t V9:{V9} \t V10:{V10} \t V11:{V11} \t V12:{V12} \t V13:{V13} \t V14:{V14} \n 
+    V15:{V15} \t V16:{V16} \t V17:{V17} \t V18:{V18} \t V19:{V19} \t V20:{V20} \t V21:{V21} \n 
+    V22:{V22} \t V23:{V23} \t V24:{V24} \t V25:{V25} \t V26:{V26} \t V27:{V27} \t V28:{V28} \n
+    Amount:{Amount}""")
 
-    res = re.post(f"http://backend.docker:8000/predict/",json=values)
-    json_str = json.dumps(res.json())
-    resp = json.loads(json_str)
-    
-    if sender_name=='' or receiver_name == '':
-        st.write("Error! Please input Transaction ID or Names of Sender and Receiver!")
+    st.subheader("Prediction:")
+    response = re.post("http://backend.docker:8000/predict", json=values)
+    if response.status_code == 200:
+        result = response.json()
+        if result is not None:
+            st.write(f"{result[0]}")
+        else:
+            st.write("Invalid response from prediction API.")
     else:
-        st.write(f"""### The '{x}' transaction that took place between {sender_name} and {receiver_name} is {resp[0]}.""")
-
-
-
-
-
-
-
-
-
+        st.write(f"Error: {response.status_code} - {response.text}")
